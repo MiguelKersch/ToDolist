5d9df71ea06adca1a53f482328fa417d0e3ceb90
Miguel Kerschkamp <99051637@mydavinci.nl>
2020-02-12 14:12:20 +0100

    added conection and started working on form

--- index.php
+++ index.php
@@ -1,31 +1,21 @@
 <html>
-<header>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <h1>ToDolist</h1>
-</header>
+<?php include "parts/navbar.php" ?>
 
 <body>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Task</th>
-                <th scope="col">Description</th>
-                <th scope="col">Requirements</th>
-                <th scope="col">Deadline</th>
-            </tr>
-        </thead>
-        <tbody>
-             <?php foreach $all as $da
-            ?>
-            <tr>
-                <th scope="row"></th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-        </tbody>
-    </table>
+    <a href="form.php">
+        <div class="row">
+            <?php foreach ($item as $todo) { ?>
+                <div class="col-6">
+                    <div class="card bg-light mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Light card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
 </body>
 
 </html>
